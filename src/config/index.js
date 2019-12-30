bb7fde215ac7eaca548f1535fa96370912ad7269
janakhpon <jnovaxer@gmail.com>
2019-12-30 11:57:22 +0630

    GDEV000

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./key-pro')
+}else{
+    module.exports = require('./key-pro')
+}
---
+++ src/models/User/index.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    position: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("User", UserSchema)
