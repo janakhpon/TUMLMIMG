0513e5144e5439c96befd599c2a4202024e09b9e
janakhpon <jnovaxer@gmail.com>
2019-12-30 16:27:32 +0630

    GDEV003

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = require('./key-pro')
 }else{
-    module.exports = require('./key-pro')
+    module.exports = require('./key-dev')
 }
